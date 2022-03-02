f9ca41acb40510e89fbe8a3931280681ac677fac
Gino Tasis <gtasis84@gmail.com>
2022-03-02 11:07:46 -0500

    Added anoher view controller for filtering factions

---
+++ Star Wars Game/Controller/FactionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FactionViewController.swift
+//  Star Wars Game
+//
+//  Created by Gino Tasis on 3/2/22.
+//
+
+import UIKit
+
+class FactionViewController: UIViewController {
+
+    
+    
+    
+}
